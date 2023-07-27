7df04910e4ed8139ddee567f8fac99b881f2d5d4
Vaibhav Shivaji Wable <57138983+wablevaibhav@users.noreply.github.com>
2023-07-27 13:20:34 +0530

    commit

--- src/main/java/com/project/employee/EmployeeApplication.java
+++ src/main/java/com/project/employee/EmployeeApplication.java
@@ -8,7 +8,7 @@ public class EmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
-		System.out.println("Started...");
+		System.out.println("Hello...");
 	}
 
 }
